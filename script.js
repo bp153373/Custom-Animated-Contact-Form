41807a8dfd104123d03484787822bf191770f975
bp153373 <bp153373@edu.rocwb.nl>
2018-05-18 14:15:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+<script type="text/javascript">
+	// Animate the Label
+// ****************************************
+
+jQuery(function() {
+  jQuery("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    jQuery(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+  }).on("focus", ".floating-label-form-group", function() {
+    jQuery(this).addClass("floating-label-form-group-with-focus");
+  }).on("blur", ".floating-label-form-group", function() {
+    jQuery(this).removeClass("floating-label-form-group-with-focus");
+  });
+});
+
+// Add .is-send class
+// ****************************************
+
+jQuery('.contact-send').click(function(){
+	jQuery(".form-send-success").css("display", "block");
+  jQuery(this).parents('.form-wrapper').addClass('is-sent');
+	
+	setTimeout(function(){
+    jQuery('.form-wrapper').removeClass('is-sent');
+  }, 3600);
+	
+});
+</script>
