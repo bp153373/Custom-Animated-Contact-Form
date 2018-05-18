5587f8cc020386203889dfa5756fc8e7702fb312
bp153373 <bp153373@edu.rocwb.nl>
2018-05-18 15:19:21 +0200

    Create script-stop.js

---
+++ script-stop.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+	// Animate the Label
+// ****************************************
+
+jQuery(function() {
+  jQuery("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    jQuery(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+  }).on("focus", ".floating-label-form-group", function() {
+    jQuery(this).addClass("floating-label-form-group-with-focus");
+  }).on("blur", ".floating-label-form-group", function() {
+    jQuery(this).removeClass("floating-label-form-group-with-focus");
+  });
+});
+
+// Add .is-send class
+// ****************************************
+
+jQuery('.contact-send').click(function(){
+  jQuery(this).parents('.form-wrapper').addClass('is-sent');
+	
+	setTimeout(function(){
+    jQuery('.form-wrapper').hide();
+  }, 600);
+	
+});
+</script>
